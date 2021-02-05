c20d9735ed53dc35b04272d26f8bdc89f18b1396
Duván Smith Montenegro Roque <programador5@sypsolutions.com>
2021-02-05 10:39:18 -0500

    Trabajo con router

--- Controllers/AsignaturaController.cs
+++ Controllers/AsignaturaController.cs
@@ -1,5 +1,6 @@
 ﻿using AspNetCore.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,11 +10,27 @@ namespace AspNetCore.Controllers
 {
     public class AsignaturaController : Controller
     {
-        public IActionResult Index()
+        
+        /*public IActionResult Index()
         {
             ViewBag.CosaDinamica = "algo";
             ViewBag.Fecha = DateTime.Now;
             return View(_context.Asignaturas.FirstOrDefault());
+        }*/
+        [Route("Asignatura/Index")]
+        [Route("Asignatura/Index/{asignaturaId}")]
+        public IActionResult Index(string asignaturaId)
+        {
+            if (!string.IsNullOrWhiteSpace(asignaturaId))
+            {
+                var a = from asi in _context.Asignaturas where asi.Id == asignaturaId select asi;
+                return View(a.SingleOrDefault());
+            }
+            else
+            {
+                return View("MultiAsignatura", _context.Asignaturas);
+            }
+
         }
         public IActionResult MultiAsignatura()
         {
